b81dd7786864bac925c0381b7f7d868726ad01b2
V1rtual <whz1006549138@gmail.com>
2026-05-08 20:15:18 +0800

    2026.5.8

---
+++ src/lc98_验证二叉搜索树/Solution.java
@@ -0,0 +1,38 @@
+package lc98_验证二叉搜索树;
+
+public class Solution {
+    public boolean isValidBST(TreeNode root) {
+
+        return dfs(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+
+    public boolean dfs(TreeNode root, int min, int max) {
+        if (root == null) {
+            return true;
+        }
+        if (!dfs(root.left, min, root.val) && !dfs(root.right, root.val, max)) {
+            return false;
+        }
+        return true;
+    }
+}
+
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}

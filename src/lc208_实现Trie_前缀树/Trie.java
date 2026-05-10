7188aabb11517c5b7b6eb25270b5d0c9d6e12e10
V1rtual <whz1006549138@gmail.com>
2026-05-10 20:32:04 +0800

    2026.5.10

---
+++ src/lc208_实现Trie_前缀树/Trie.java
@@ -0,0 +1,20 @@
+package lc208_实现Trie_前缀树;
+
+class Trie {
+
+    public Trie() {
+
+    }
+
+    public void insert(String word) {
+
+    }
+
+    public boolean search(String word) {
+
+    }
+
+    public boolean startsWith(String prefix) {
+
+    }
+}
